90f363579f1578f15f625ed2cf598ecdc0f78d73
nanannnnnn <crimson.2021.iwasaki.tomoya@gmail.com>
2025-08-31 09:16:29 +0900

    style: ボタンのデザインを調整

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -6,7 +6,9 @@ function Content() {
     return (
         <main className="ml-64 min-h-screen bg-white">
             <div className="max-w-4xl mx-auto px-6 py-8">
-                <button onClick={onClickGetSomething} className="bg-black text-white">Get Something</button>
+                <button onClick={onClickGetSomething} className="primary-button">
+                    Get Something
+                </button>
                 {result && (
                     <div className="mt-4">
                         <h3 className="font-bold text-xl">title: {result.title}</h3>
